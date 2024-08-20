8c61ff40ad5e328b65e154fd61b1019f5b91d01f
Cat Games Inc <148311731+Cat-Games@users.noreply.github.com>
2024-08-20 11:17:54 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
 document.addEventListener("DOMContentLoaded", () =>{
     const container = document.getElementById('container')
     const darkbutton = document.getElementById('darkToggle')
-
+    const darkToggleButton = document.gelElementById('darkToggleButton')
+    
     function darkModeToggle(){
         document.body.classList.toggle('dark-mode')
         container.classList.toggle('dark-mode')
+
+        if(document.body.classList.Contain('dark-mode')){
+            darkToggleButton.textContainer = "Toggle Light Mode"
+        }else{
+            darkToggleButton.textContainer = "Toggle Dark Mode"
+        }
     }
 
     const prefersDark = window.matchMedia('(prefers-color-scheme: dark)')
     if(prefersDark.matches){
+        darkModeToggle()
+    }
+
+    darkToggleButton.onlick = function(){
         darkModeToggle()
     }
 
